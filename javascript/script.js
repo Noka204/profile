f13f070a9a5fcc3663d3b8cb7c8058d55c42d818
Noka204 <Khanhabcdt@gmail.com>
2025-02-24 08:58:39 +0700

    view eye

--- javascript/script.js
+++ javascript/script.js
@@ -26,14 +26,17 @@ document.addEventListener("DOMContentLoaded", function () {
         video.volume = 0.8; // Đặt mức âm lượng mặc định là 80%
     });
 });
-if (!localStorage.getItem('viewCount')) {
-    localStorage.setItem('viewCount', '0');
-  }
-  // Lấy số lượt view hiện tại và chuyển đổi thành số
-  let count = parseInt(localStorage.getItem('viewCount'), 10);
-  // Tăng số lượt view lên 1
-  count++;
-  // Lưu lại số lượt view mới vào localStorage
-  localStorage.setItem('viewCount', count.toString());
-  // Cập nhật hiển thị trên trang
-  document.getElementById('view-count').textContent = count;
+document.addEventListener('DOMContentLoaded', function() {
+    if (!localStorage.getItem('viewCount')) {
+      localStorage.setItem('viewCount', '0');
+    }
+    // Lấy số lượt view hiện tại và chuyển đổi thành số
+    let count = parseInt(localStorage.getItem('viewCount'), 10);
+    // Tăng số lượt view lên 1
+    count++;
+    // Lưu lại số lượt view mới vào localStorage
+    localStorage.setItem('viewCount', count.toString());
+    // Cập nhật hiển thị trên trang
+    document.getElementById('view-count').textContent = count;
+  });
+  
